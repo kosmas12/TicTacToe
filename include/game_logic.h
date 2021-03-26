a5a3678e3e16769d34926f146ec6ee49cb594e05
Teufelchen1 <bennet.blischke@outlook.com>
2021-03-26 09:38:37 +0100

    wip: Adding test

---
+++ include/game_logic.h
@@ -0,0 +1,24 @@
+/*
+This file is part of TicTacToe.
+
+TicTacToe is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TicTacToe is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TicTacToe.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef TICTACTOE_GAME_LOGIC_H
+#define TICTACTOE_GAME_LOGIC_H
+
+char checkForThree(char a, char b, char c);
+char findWinner(char map[3][3]);
+
+#endif //TICTACTOE_GAME_LOGIC_H
